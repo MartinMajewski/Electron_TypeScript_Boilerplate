6e3b28053261dda9b9d6024daf874160001576cb
Martin Majewski <martinmajewski@PC2276.local>
2016-12-16 20:45:39 +0100

    Deleted build folder. It will be ignored in further commits.

--- build/App.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const electron_1 = require("electron");
-const Main_1 = require("./Main");
-Main_1.default.main(electron_1.app, electron_1.BrowserWindow);
-//# sourceMappingURL=App.js.map
